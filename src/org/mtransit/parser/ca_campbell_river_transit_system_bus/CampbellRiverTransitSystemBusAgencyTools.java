e7f985747b7868ea8a495dc9d3b3994135ab8cac
Mathieu Méa <mathieu.mea@gmail.com>
2017-08-25 19:18:32 -0400

    Compatibility with latest update.

--- src/org/mtransit/parser/ca_campbell_river_transit_system_bus/CampbellRiverTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_campbell_river_transit_system_bus/CampbellRiverTransitSystemBusAgencyTools.java
@@ -1,7 +1,6 @@
 package org.mtransit.parser.ca_campbell_river_transit_system_bus;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -22,7 +21,6 @@ import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
 import org.mtransit.parser.mt.data.MTripStop;
@@ -52,13 +50,8 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 		System.out.printf("\nGenerating Campbell River Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
-	private static final String INCLUDE_ONLY_SERVICE_ID_CONTAINS = null;
-
 	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
-		if (INCLUDE_ONLY_SERVICE_ID_CONTAINS != null && !gCalendar.getServiceId().contains(INCLUDE_ONLY_SERVICE_ID_CONTAINS)) {
-			return true;
-		}
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
 		}
@@ -67,9 +60,6 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 
 	@Override
 	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
-		if (INCLUDE_ONLY_SERVICE_ID_CONTAINS != null && !gCalendarDates.getServiceId().contains(INCLUDE_ONLY_SERVICE_ID_CONTAINS)) {
-			return true;
-		}
 		if (this.serviceIds != null) {
 			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
 		}
@@ -88,9 +78,6 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 
 	@Override
 	public boolean excludeTrip(GTrip gTrip) {
-		if (INCLUDE_ONLY_SERVICE_ID_CONTAINS != null && !gTrip.getServiceId().contains(INCLUDE_ONLY_SERVICE_ID_CONTAINS)) {
-			return true;
-		}
 		if (this.serviceIds != null) {
 			return excludeUselessTrip(gTrip, this.serviceIds);
 		}
@@ -155,137 +142,6 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
 	static {
 		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
-		map2.put(1L, new RouteTripSpec(1L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // Community Centre Exchange
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"112000", // == North Isl College
-								"110765", // != Hilchey @ Trilliu
-								"110767", // != Hilchey @ S Alder
-								"110969", // != S Alder @ Pacific Hts
-								"110924", // != S Alder @ Goodwin
-								"110768", // == S Alder @ Pkwy
-								"110774", // ++
-								"111001", // Community Centre Exchange
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"111001", // Community Centre Exchange
-								"110756", // ++
-								"112000", // == North Isl College
-						})) //
-				.compileBothTripSort());
-		map2.put(2L, new RouteTripSpec(2L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // Community Centre Exchange
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"110945", // S Alder @ Nodales
-								"110994", // ++
-								"111002", // Campbell River Community Centre Exchange Bay B
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"111002", // Campbell River Community Centre Exchange Bay B
-								"110790", // ++
-								"110945", // S Alder @ Nodales
-						})) //
-				.compileBothTripSort());
-		map2.put(4L, new RouteTripSpec(4L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) // Community Centre Exchange
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"111003", // Campbell River Community Centre Exchange Bay D
-								"110857", // ++
-								"110865", // Discovery @ Wood
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"110865", // Discovery @ Wood
-								"110874", // ++
-								"111003", // Campbell River Community Centre Exchange Bay D
-						})) //
-				.compileBothTripSort());
-		map2.put(5L, new RouteTripSpec(5L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // Community Centre Exchange
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"110936", // S Alder @ Hudson
-								"110939", // Southbound Meadowbrook at Pine
-
-								"110886", // == Niluht @ Superior
-								"110887", // != Shellbourne @ Sandowne
-								"110890", // != Robron @ Upland
-								"110772", // != S Alder @ Frances
-								"110773", // == S Alder @ Robron
-								"110774", // != S Alder @ Albatross
-								"110777", // != Alder @ Evergreen
-								"110978", // != 1st Ave @ Thulin
-								"110778", // Alder @ Coronation
-
-								"110780", // == Alder @ 4th Ave
-								"112019", // != Alder @ 6th
-								"111000", // == Alder @ 9th Ave
-								"111001", // Community Centre Exchange
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"111002", // Campbell River Community Centre Exchange Bay B
-								"110988", // ++
-								"110936", // S Alder @ Hudson
-						})) //
-				.compileBothTripSort());
-		map2.put(15L, new RouteTripSpec(15L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // North Island College
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) // Homalco
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"112002", // 1218 Bute Cres
-								"110946", // ++
-								"110800", // ++
-								"112000", // North Island College
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"112000", // North Island College
-								"112002", // 1218 Bute Cres
-						})) //
-				.compileBothTripSort());
-		map2.put(16L, new RouteTripSpec(16L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), // Community Centre Exchange
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList(new String[] { //
-						"112004", // Eagle @ Seahawk
-								"110880", // ++
-								"110781", // Campbell River Community Centre Exchange Bay A
-						})) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList(new String[] { //
-						"111002", // Campbell River Community Centre Exchange Bay B
-								"110854", // ++
-								"112004", // Eagle @ Seahawk
-						})) //
-				.compileBothTripSort());
-		map2.put(99L, new RouteTripSpec(99L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), // Community Centre Exchange
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"110923", // S Alder @ Holm
-								"110774", // ++
-								"111001", // Community Centre Exchange
-						})) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(new String[] { //
-						"110755", // Southbound Dogwood at 7th Ave
-								"110762", // ++
-								"110923", // S Alder @ Holm
-						})) //
-				.compileBothTripSort());
 		ALL_ROUTE_TRIPS2 = map2;
 	}
 
@@ -320,16 +176,16 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 		}
 		if (mRoute.getId() == 7L) {
 			if (gTrip.getDirectionId() == 0) {
-				if (StringUtils.isEmpty(gTrip.getTripHeadsign()) || "Petersen".equalsIgnoreCase(gTrip.getTripHeadsign())) {
-					mTrip.setHeadsignStringNotEmpty("CW", gTrip.getDirectionId());
+				if (StringUtils.isEmpty(gTrip.getTripHeadsign()) || "Petersen A.M. Loop".equalsIgnoreCase(gTrip.getTripHeadsign())) {
+					mTrip.setHeadsignStringNotEmpty("AM", gTrip.getDirectionId());
 					return;
 				} else {
 					System.out.printf("\nUnexpected route trip to set for %s!\n", gTrip);
 					System.exit(-1);
 				}
 			} else if (gTrip.getDirectionId() == 1) {
-				if (StringUtils.isEmpty(gTrip.getTripHeadsign())) {
-					mTrip.setHeadsignStringNotEmpty("CCW", gTrip.getDirectionId());
+				if (StringUtils.isEmpty(gTrip.getTripHeadsign()) || "Petersen P.M. Loop".equalsIgnoreCase(gTrip.getTripHeadsign())) {
+					mTrip.setHeadsignStringNotEmpty("PM", gTrip.getDirectionId());
 					return;
 				} else {
 					System.out.printf("\nUnexpected route trip to set for %s!\n", gTrip);
@@ -343,6 +199,7 @@ public class CampbellRiverTransitSystemBusAgencyTools extends DefaultAgencyTools
 	private static final String EXCH = "Exch";
 	private static final Pattern EXCHANGE = Pattern.compile("((^|\\W){1}(exchange)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
 	private static final String EXCHANGE_REPLACEMENT = "$2" + EXCH + "$4";
+
 	private static final Pattern ENDS_WITH_EXPRESS = Pattern.compile("( express.*$)", Pattern.CASE_INSENSITIVE);
 
 	private static final Pattern ENDS_WITH_LOCAL = Pattern.compile("( local.*$)", Pattern.CASE_INSENSITIVE);
